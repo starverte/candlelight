f93764e2d1b6790902e80040c1fb4ac74dcf0044
Matt Beall <mbeall@starverte.com>
2014-04-25 12:06:00 -0600

    Create layout for front page

--- front-page.php
+++ front-page.php
@@ -12,34 +12,177 @@
  */
 
 get_header(); ?>
-<?php flint_get_widgets('header'); ?>
 
-  <section class="hero">
-  	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/hero.png" />
-  	<div class="container">
-    	
-    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    	<p>Some content</p>
-    </div>
-    </div>
-  </section><!-- .hero -->
-  
-  <section id="problem" class="canvas-red container">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    	<p>Some content</p>
-    </div>
-  </section><!-- #problem .canvas-red -->
-  
-  <section id="what-we-do" class="canvas-green container">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    	<p>Some content</p>
-    </div>
-  </section><!-- #what-we-do .canvas-green -->
-  
-  <section id="get-involved" class="canvas-orange container">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    	<p>Some content</p>
-    </div>
-  </section><!-- #get-involved .canvas-orange -->
+  <section id="intro">
+
+    <div class="hero">
+
+      <div class="container">
+
+        <div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+          <p>Vuyiroli is a non-profit<br>
+            organization that serves<br>
+            the abandoned elderly in need,<br>
+            regardless of caste, creed,<br>
+            religion, or language.</p>
+        </div><!-- .col-lg-12 -->
+
+      </div><!-- .container -->
+
+    </div><!-- #intro .hero -->
+
+    <div class="canvas-white container visible-xs">
+
+        <div class="col-xs-12">
+          <p>Vuyiroli is a non-profit<wbr>
+            organization that serves<wbr>
+            the abandoned elderly in need,<wbr>
+            regardless of caste, creed,
+            religion,<wbr> or language.</p>
+        </div><!-- .col-xs-12 -->
+
+    </div><!-- .canvas-white -->
+
+  </section><!-- #intro -->
+
+  <section id="problem">
+
+    <div class="hero">
+
+      <div class="container">
+
+        <div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+
+          <h1>Abandoned</h1>
+
+          <p>by their families,<br>
+            elders are left on streets<br>
+            with no home,<br>
+            no food and no support</p>
+
+        </div><!-- .col-lg-12 -->
+
+      </div><!-- .container -->
+
+    </div><!-- .hero -->
+
+    <div class="canvas-white container visible-xs text-red">
+
+        <div class="col-xs-12">
+
+          <h1>Abandoned</h1>
+
+          <p>by their families,<wbr>
+            elders are left on streets<wbr>
+            with no home,<wbr>
+            no food and no support</p>
+
+        </div><!-- .col-xs-12 -->
+
+    </div><!-- .canvas-white -->
+
+    <div class="canvas-white text-red">
+
+      <div class="container">
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p>leaving elders alone and hurting</p>
+        </div><!-- .col-lg-12 -->
+
+      </div><!-- .container -->
+
+    </div><!-- .canvas-white -->
+
+    <div class="canvas-red">
+
+      <div class="container">
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p>Problem</p>
+        </div><!-- .col-lg-12 -->
+
+      </div><!-- .container -->
+
+    </div><!-- .canvas-red -->
+
+    <div class="canvas-white text-red">
+      <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p>so families abandon them</p>
+        </div><!-- .col-lg-12 -->
+      </div><!-- .container -->
+    </div><!-- .canvas-white -->
+
+  </section><!-- #problem -->
+
+  <section id="what-we-do">
+
+    <div class="hero">
+
+      <div class="container">
+
+        <div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+
+          <h1>Rescued</h1>
+
+          <p>by Vuyiroli, elders have a home,<br>
+            food and community</p>
+
+        </div><!-- .col-lg-12 -->
+
+      </div><!-- .container -->
+
+    </div><!-- .hero -->
+
+    <div class="canvas-white container visible-xs text-green">
+
+        <div class="col-xs-12">
+
+          <h1>Rescued</h1>
+
+          <p>by Vuyiroli, elders have a home,<wbr>
+            food and community</p>
+
+        </div><!-- .col-xs-12 -->
+
+    </div><!-- .canvas-white -->
+
+    <div class="canvas-green">
+      <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p>Lorem Ipsum</p>
+        </div><!-- .col-lg-12 -->
+      </div><!-- .container -->
+    </div><!-- .canvas-green -->
+
+    <div class="canvas-white">
+      <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p>Lorem Ipsum</p>
+        </div><!-- .col-lg-12 -->
+      </div><!-- .container -->
+    </div><!-- .canvas-white -->
+
+  </section><!-- #what-we-do -->
+
+  <section id="get-involved">
+
+    <div class="canvas-orange">
+      <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p>Lorem Ipsum</p>
+        </div><!-- .col-lg-12 -->
+      </div><!-- .container -->
+    </div><!-- .canvas-orange -->
+
+    <div class="canvas-white">
+      <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p>Lorem Ipsum</p>
+        </div><!-- .col-lg-12 -->
+      </div><!-- .container -->
+    </div><!-- .canvas-white -->
+
+  </section><!-- #get-involved -->
 
 <?php get_footer(); ?>
