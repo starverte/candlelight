b9db3f6af606b87858be5426957e5025e2649343
Matt Beall <mbeall@starverte.com>
2014-04-28 01:39:06 -0600

    Complete front-page "template"

--- front-page.php
+++ front-page.php
@@ -19,21 +19,21 @@ get_header(); ?>
 
       <div class="container">
 
-        <div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
-          <p>Vuyiroli is a non-profit<br>
+        <div class="col-lg-12 col-md-12 hidden-sm hidden-xs">
+          <span class="text"><p>Vuyiroli is a non-profit<br>
             organization that serves<br>
             the abandoned elderly in need,<br>
             regardless of caste, creed,<br>
-            religion, or language.</p>
+            religion, or language.</p></span>
         </div><!-- .col-lg-12 -->
 
       </div><!-- .container -->
 
     </div><!-- #intro .hero -->
 
-    <div class="canvas-white container visible-xs">
+    <div class="canvas-white container visible-sm visible-xs">
 
-        <div class="col-xs-12">
+        <div class="col-sm-12 col-xs-12">
           <p>Vuyiroli is a non-profit<wbr>
             organization that serves<wbr>
             the abandoned elderly in need,<wbr>
@@ -42,6 +42,8 @@ get_header(); ?>
         </div><!-- .col-xs-12 -->
 
     </div><!-- .canvas-white -->
+    
+    <div class="canvas-white text-red hidden-sm hidden-xs text-center">Learn more <i class="fa fa-chevron-down"></i></div>
 
   </section><!-- #intro -->
 
@@ -51,14 +53,18 @@ get_header(); ?>
 
       <div class="container">
 
-        <div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
-
-          <h1>Abandoned</h1>
-
-          <p>by their families,<br>
-            elders are left on streets<br>
-            with no home,<br>
-            no food and no support</p>
+        <div class="col-lg-12 col-md-12 hidden-sm hidden-xs">
+        
+          <span class="text">
+          
+            <h1>Abandoned</h1>
+  
+            <p>by their families,<br>
+              elders are left on streets<br>
+              with no home,<br>
+              no food and no support</p>
+              
+          </span>
 
         </div><!-- .col-lg-12 -->
 
@@ -66,9 +72,9 @@ get_header(); ?>
 
     </div><!-- .hero -->
 
-    <div class="canvas-white container visible-xs text-red">
+    <div class="canvas-white container visible-sm visible-xs text-red">
 
-        <div class="col-xs-12">
+        <div class="col-sm-12 col-xs-12">
 
           <h1>Abandoned</h1>
 
@@ -76,16 +82,17 @@ get_header(); ?>
             elders are left on streets<wbr>
             with no home,<wbr>
             no food and no support</p>
+            <p>leaving elders alone and hurting</p>
 
         </div><!-- .col-xs-12 -->
 
     </div><!-- .canvas-white -->
 
-    <div class="canvas-white text-red">
+    <div class="canvas-white text-red hidden-sm hidden-xs">
 
       <div class="container">
 
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-lg-12 col-md-12">
           <p>leaving elders alone and hurting</p>
         </div><!-- .col-lg-12 -->
 
@@ -93,13 +100,119 @@ get_header(); ?>
 
     </div><!-- .canvas-white -->
 
-    <div class="canvas-red">
+    <div class="canvas-red infograph">
 
       <div class="container">
 
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <p>Problem</p>
-        </div><!-- .col-lg-12 -->
+        <div class="row">
+
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1>In a country without</h1>
+          </div><!-- .col-lg-12 -->
+        
+        </div>
+        
+        <div class="spacer"></div>
+        
+        <div class="row">
+        
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 txt-center">
+            <i class="fa fa-hospital-o fa-7x"></i>
+            <h2>Medicare</h2>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 txt-center">
+            <i class="fa fa-money fa-7x"></i>
+            <h2>Social Security</h2>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 txt-center">
+            <i class="fa fa-shield fa-7x"></i>
+            <h2>or other social safety nets</h2>
+          </div><!-- .col-lg-4 -->
+        
+        </div><!-- .row -->
+        
+        <div class="spacer"></div>
+        
+        <div class="row">
+          
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 txt-center">
+            <i class="fa fa-wheelchair fa-10x"></i>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 txt-center">
+            <h1>Elders in India with illness or disability cannot support themselves</h1>
+          </div><!-- .col-lg-4 -->
+        
+        </div><!-- .row -->
+        
+        <div class="spacer"></div>
+        
+        <div class="row">
+
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1>Families in India are changing</h1>
+          </div><!-- .col-lg-12 -->
+        
+        </div>
+        
+        <div class="spacer"></div>
+        
+        <div class="row">
+        
+          <div class="col-lg-2 col-md-1"></div>
+          
+          <div class="col-lg-3 col-md-4 col-sm-5 col-xs-12 txt-center">
+            <img class="max-215" src="<?php echo get_stylesheet_directory_uri(); ?>/img/extended-family.png" />
+            <h2>Extended families</h2>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-2 col-md-2 col-sm-2"></div>
+          
+          <div class="visible-xs spacer"></div>
+          
+          <div class="col-lg-3 col-md-4 col-sm-5 col-xs-12 txt-center">
+            <img class="max-215" src="<?php echo get_stylesheet_directory_uri(); ?>/img/nuclear-family.png" />
+            <h2>...are now nuclear</h2>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-2 col-md-1"></div>
+        
+        </div><!-- .row -->
+        
+        <div class="spacer"></div>
+        
+        <div class="row">
+
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h2>Elders are seen as liabilities because they</h2>
+          </div><!-- .col-lg-12 -->
+        
+        </div>
+        
+        <div class="spacer"></div>
+        
+        <div class="row">
+        
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 txt-center">
+            <i class="fa fa-user-md fa-7x"></i>
+            <h2>create high medical costs</h2>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 txt-center">
+            <i class="fa fa-home fa-7x"></i>
+            <h2>increase housing costs</h2>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 txt-center">
+            <i class="fa fa-plane fa-7x"></i>
+            <h2>and limit the pursuit of global opportunities</h2>
+          </div><!-- .col-lg-4 -->
+        
+        </div><!-- .row -->
+        
+        <div class="spacer"></div>
 
       </div><!-- .container -->
 
@@ -121,12 +234,15 @@ get_header(); ?>
 
       <div class="container">
 
-        <div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+        <div class="col-lg-12 col-md-12 hidden-sm hidden-xs">
 
+          <span class="text">
           <h1>Rescued</h1>
 
           <p>by Vuyiroli, elders have a home,<br>
             food and community</p>
+          
+          </span>
 
         </div><!-- .col-lg-12 -->
 
@@ -134,54 +250,155 @@ get_header(); ?>
 
     </div><!-- .hero -->
 
-    <div class="canvas-white container visible-xs text-green">
+    <div class="canvas-white container visible-sm visible-xs text-green">
 
-        <div class="col-xs-12">
+        <div class="col-sm-12 col-xs-12">
 
           <h1>Rescued</h1>
 
           <p>by Vuyiroli, elders have a home,<wbr>
             food and community</p>
+          <p>bringing light to those in the dark</p>
 
         </div><!-- .col-xs-12 -->
 
     </div><!-- .canvas-white -->
+    
+    <div class="canvas-white text-green hidden-sm hidden-xs">
 
-    <div class="canvas-green">
       <div class="container">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <p>Lorem Ipsum</p>
+
+        <div class="col-lg-12 col-md-12">
+          <p>bringing light to those in the dark</p>
         </div><!-- .col-lg-12 -->
+
+      </div><!-- .container -->
+
+    </div><!-- .canvas-white -->
+
+    <div class="canvas-green infograph">
+      <div class="container">
+      
+        <div class="row">
+
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1 class="text-center">Vuyiroli provides</h1>
+          </div><!-- .col-lg-12 -->
+        
+        </div>
+        
+        <div class="spacer"></div>
+      
+        <div class="row">
+        
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 txt-center">
+            <i class="fa fa-home fa-7x"></i>
+            <h2>a permanent home</h2>
+          </div><!-- .col-lg-3 -->
+          
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 txt-center">
+            <i class="fa fa-cutlery fa-7x"></i>
+            <h2>daily food</h2>
+          </div><!-- .col-lg-3 -->
+          
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 txt-center">
+            <i class="fa fa-medkit fa-7x"></i>
+            <h2>medicine</h2>
+          </div><!-- .col-lg-3 -->
+          
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 txt-center">
+            <i class="fa fa-eye fa-7x"></i>
+            <h2>eye exams and glasses</h2>
+          </div><!-- .col-lg-3 -->
+        
+        </div><!-- .row -->
+        
+        <div class="spacer"></div>
+        
+        <div class="row">
+
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1 class="text-center hidden-xs">Vuyiroli helps them live out their remaining years with dignity, companionship, and happiness</h1>
+            <h2 class="text-center visible-xs">Vuyiroli helps them live out their remaining years with dignity, companionship, and happiness</h2>
+          </div><!-- .col-lg-12 -->
+        
+        </div>
+        
+        <div class="spacer"></div>
+        
       </div><!-- .container -->
     </div><!-- .canvas-green -->
 
-    <div class="canvas-white">
-      <div class="container">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <p>Lorem Ipsum</p>
-        </div><!-- .col-lg-12 -->
-      </div><!-- .container -->
-    </div><!-- .canvas-white -->
+    <div class="canvas-white"></div>
 
   </section><!-- #what-we-do -->
 
   <section id="get-involved">
 
-    <div class="canvas-orange">
+    <div class="canvas-orange infograph">
       <div class="container">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <p>Lorem Ipsum</p>
-        </div><!-- .col-lg-12 -->
+      
+        <div class="spacer"></div>
+        
+        <div class="row hidden-sm">
+        
+          <div class="col-lg-4 col-md-4 col-xs-12 txt-center">
+            <i class="fa fa-money fa-7x"></i>
+            <h1>Donate</h1>
+            <p>$3 a day provides one elder with shelter, food, medicine, and more.</p>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-4 col-md-4 col-xs-12 txt-center">
+            <i class="fa fa-refresh fa-7x"></i>
+            <h1>Follow</h1>
+            <p>Learn more about Vuyiroli, the residents at Vuyiroli, and more ways to get involved.</p>
+          </div><!-- .col-lg-4 -->
+          
+          <div class="col-lg-4 col-md-4 col-xs-12 txt-center">
+            <i class="fa fa-share fa-7x"></i>
+            <h1>Share</h1>
+            <p>Share with others the problem in India, and how they can help.</p>
+          </div><!-- .col-lg-4 -->
+        
+        </div><!-- .row -->
+        
+        <div class="row visible-sm">
+        
+          <div class="col-sm-4">
+            <i class="fa fa-money fa-7x"></i>
+          </div><!-- .col-sm-4 -->
+          <div class="col-sm-8">
+            <h2>Donate</h2>
+            <p>$3 a day provides one elder with shelter, food, medicine, and more.</p>
+          </div><!-- .col-sm-8 -->
+          
+          <div class="spacer"></div>
+          
+          <div class="col-sm-4">
+            <i class="fa fa-refresh fa-7x"></i>
+          </div><!-- .col-sm-4 -->
+          <div class="col-sm-8">
+            <h2>Follow</h2>
+            <p>Learn more about Vuyiroli, the residents at Vuyiroli, and more ways to get involved.</p>
+          </div><!-- .col-sm-8 -->
+          
+          <div class="spacer"></div>
+          
+          <div class="col-sm-4">
+            <i class="fa fa-share fa-7x"></i>
+          </div><!-- .col-sm-4 -->
+          <div class="col-sm-8">
+            <h2>Share</h2>
+            <p>Share with others the problem in India, and how they can help.</p>
+          </div><!-- .col-sm-8 -->
+        
+        </div><!-- .row -->
+        
+        <div class="spacer"></div>
+        
       </div><!-- .container -->
-    </div><!-- .canvas-orange -->
 
-    <div class="canvas-white">
-      <div class="container">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <p>Lorem Ipsum</p>
-        </div><!-- .col-lg-12 -->
-      </div><!-- .container -->
-    </div><!-- .canvas-white -->
+    </div><!-- .canvas-orange -->
 
   </section><!-- #get-involved -->
 
