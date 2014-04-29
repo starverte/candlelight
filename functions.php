e5ae7d4c96e53e4604dced3bb13c7c3db77bf3f2
Matt Beall <mbeall@starverte.com>
2014-04-29 03:04:29 -0600

    Add support for custom elder fields

--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@
 
 function cl_enqueue_scripts() {
   
+  require( get_stylesheet_directory() . '/inc/template-tags.php' );
+  
   /**
    * Load Font Awesome
    */
@@ -14,4 +16,18 @@ function cl_enqueue_scripts() {
 
 }
 add_action( 'wp_enqueue_scripts', 'cl_enqueue_scripts', 20 );
+
+add_action('steel_teams_add_meta','cl_add_teams_meta');
+function cl_add_teams_meta() {
+  echo '<p><label>Native Language</label><br /><input type="text"  size="25" name="profile_lang" value="' . steel_meta('profile','lang') . '" /></p>';
+  echo '<p><label>Native Area</label><br /><input type="text"  size="25" name="profile_home" value="' . steel_meta('profile','home') . '" /></p>';
+  echo '<p><label>Physical Ailments</label><br /><input type="text"  size="25" name="profile_ailments" value="' . steel_meta('profile','ailments') . '" /></p>';
+}
+add_action('steel_teams_save_meta','cl_save_teams_meta');
+function cl_save_teams_meta() {
+  global $post;
+  if (isset($_POST['profile_lang'])) { update_post_meta($post->ID, "profile_lang", $_POST["profile_lang"]); }
+  if (isset($_POST['profile_home'])) { update_post_meta($post->ID, "profile_home", $_POST["profile_home"]); }
+  if (isset($_POST['profile_ailments'])) { update_post_meta($post->ID, "profile_ailments", $_POST["profile_ailments"]); }
+}
 ?>
---
+++ inc/template-tags.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Custom template tags for this theme.
+ *
+ * @package Flint/Candlelight
+ * @since 0.0.0
+ */
+
+remove_action('flint_entry_meta_above_steel_profile','steel_profile_entry_meta_above',10);
+add_action('flint_entry_meta_above_steel_profile','cl_profile_entry_meta_above',10);
+function cl_profile_entry_meta_above() {
+  $lang     = steel_profile_meta( 'lang'    );
+  $home     = steel_profile_meta( 'home'    );
+  $ailments = steel_profile_meta( 'ailments');
+  
+  $output  = '';
+  $output .= !empty($lang) | !empty($home) | !empty($ailments) ? '<p>' : '';
+  $output .= !empty($lang) ? '<i class="glyphicon glyphicon-globe"></i> ' . $lang : '';
+  $output .= !empty($lang) && !empty($home) ? '<br>' : '';
+  $output .= !empty($home) ? '<i class="glyphicon glyphicon-home"></i> ' . $home : '';
+  $output .= !empty($home) && !empty($ailments) ? '<br>' : '';
+  $output .= !empty($ailments) ? '<i class="glyphicon glyphicon-heart"></i> ' . $ailments : '';
+  $output .= !empty($lang) | !empty($home) | !empty($ailments) ? '</p>' : '';
+  echo $output;
+}
