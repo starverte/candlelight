ed5d75e2af79ddfdf9bf894336c9a49296db9ce6
Matt Beall <mbeall@starverte.com>
2014-04-29 13:27:47 -0600

    Add buttons to get-involved section

--- front-page.php
+++ front-page.php
@@ -346,18 +346,23 @@ get_header(); ?>
             <i class="fa fa-money fa-7x"></i>
             <h1>Donate</h1>
             <p>$3 a day provides one elder with shelter, food, medicine, and more.</p>
+            <a class="btn btn-lg btn-outline-inverse steel-tooltip" href="https://www.worldventure.com/page.aspx?pid=984&fid=65135&fdesc=Vuyiroli%20Home%20for%20the%20Elderly%20Destitute-6473913" data-toggle="tooltip" data-placement="bottom" title="Donations are handled by WorldVenture" target="_blank">Donate now</a> <a class="btn btn-lg btn-outline-inverse steel-tooltip" href="https://www.worldventure.com/page.aspx?pid=982&fid=65135&fdesc=Vuyiroli%20Home%20for%20the%20Elderly%20Destitute-6473913" data-toggle="tooltip" data-placement="bottom" title="Donations are handled by WorldVenture" target="_blank">Make a commitment</a>
           </div><!-- .col-lg-4 -->
           
           <div class="col-lg-4 col-md-4 col-xs-12 txt-center">
             <i class="fa fa-refresh fa-7x"></i>
             <h1>Follow</h1>
             <p>Learn more about Vuyiroli, the residents at Vuyiroli, and more ways to get involved.</p>
+            <a class="btn btn-lg btn-outline-inverse">Subscribe to newsletter</a>
           </div><!-- .col-lg-4 -->
           
           <div class="col-lg-4 col-md-4 col-xs-12 txt-center">
             <i class="fa fa-share fa-7x"></i>
             <h1>Share</h1>
             <p>Share with others the problem in India, and how they can help.</p>
+            <a class="btn btn-lg btn-outline-inverse" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()); ?>" target="_blank">Share on Facebook</a>
+            <a class="btn btn-lg btn-outline-inverse" href="https://twitter.com/share?url=<?php echo urlencode(get_the_permalink()); ?>&text=Vuyiroli is a non-profit organization that serves the abandoned elderly of India in need.">Tweet</a>
+            
           </div><!-- .col-lg-4 -->
         
         </div><!-- .row -->
