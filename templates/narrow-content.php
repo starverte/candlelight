2a1aa0af3e6da179dc6923b7dae77d77b9946aff
Matt Beall <mbeall@starverte.com>
2015-10-08 14:27:57 -0600

    Add support for Flint 1.5

--- templates/narrow-content.php
+++ templates/narrow-content.php
@@ -11,7 +11,7 @@ $type = get_post_type(); ?>
   <div class="row">
     <div class="col-lg-1 col-md-1 col-sm-1"></div>
     <div class="col-lg-2 col-md-2 col-sm-2">
-      <?php if (is_singular()) { flint_post_thumbnail('page'); } else { flint_post_thumbnail( 'page', 'archive' ); } ?>
+      <?php if (is_singular()) { flint_the_post_thumbnail('page'); } else { flint_the_post_thumbnail( 'page', 'archive' ); } ?>
     </div>
     <article id="post-<?php the_ID(); ?>" <?php post_class('col-lg-6 col-md-6 col-sm-6'); ?>>
 
